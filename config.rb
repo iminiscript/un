0b20622c318376bc299f8a60cabba65613dcb2c4
monkviper <surinder@anattadesign.com>
2014-10-17 20:48:44 +0530

    integrate font-awesome

--- config.rb
+++ config.rb
@@ -1,11 +1,13 @@
 # Require any additional compass plugins here.
+require 'font-awesome-sass'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
-sass_dir = "scss"
-images_dir = "images"
-javascripts_dir = "javascripts"
+css_dir = "assets/css"
+sass_dir = "assets/scss"
+images_dir = "assets/images"
+fonts_dir = "assets/fonts"
+javascripts_dir = "assets/javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
